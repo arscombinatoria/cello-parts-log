927d5795d1b8651682106f68b3462e2d220fc61f
arscombinatoria <harpoon.1942.mercury+github@gmail.com>
2025-12-12 22:53:13 +0900

    Upgrade ESLint to v9

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = [
+  {
+    ignores: ['build/', 'node_modules/'],
+  },
+  ...compat.config({
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    plugins: ['react'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  }),
+];
